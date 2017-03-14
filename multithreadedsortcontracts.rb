2da2ef5450909ce6512ab27e8090504a85fd6759
aaronphilips <aphilips@ualberta.ca>
2017-03-13 19:53:35 -0600

    seems finished, but having trouble sorting large arrays

---
+++ multithreadedsortcontracts.rb
@@ -0,0 +1,21 @@
+def pre_merge(ordered_left_array, ordered_right_array)
+	assert_respond_to(ordered_left_array, :size, "merging left array, but not an array")
+	assert_respond_to(ordered_right_array, :size, "merging right array, but not an array")
+end
+
+def post_merge(ordered_left_array, ordered_right_array,return_array)
+	original_checksum = ordered_left_array.inject(0){|sum,x| sum+x} + ordered_right_array.inject(0){|sum,x| sum+x}
+	result_checksum = return_array.inject(0){|sum,x| sum+x}
+	assert original_checksum == result_checksum, 'merge failed, missing objects'
+end
+
+def pre_split(output_array)
+  assert_respond_to(output_array, :size, "splitting non array")
+end
+
+def post_split(left_array, right_array, output_array)
+  assert((left_array.size + right_array.size) ==output_array.size, "split failed")
+  original_checksum = left_array.inject(0){|sum,x| sum+x} + right_array.inject(0){|sum,x| sum+x}
+  result_checksum = output_array.inject(0){|sum,x| sum+x}
+  assert original_checksum == result_checksum, 'split failed'
+end
--- test.rb
+++ test.rb
@@ -1,7 +1,10 @@
-require "./MultiThreadedSort"
 
-using MultiThreadedSort
+require_relative 'ArrayMultithreadedSorting'
 
+using ArrayMultithreadedSorting
+include Test::Unit::Assertions
+# a=[4,2,3,4,1289,46,324,-234]
+# a.m_merge_sort(1000)
 # a.merge_sort
 # puts len
 
@@ -14,13 +17,19 @@ puts time
 array_of_objects = Array.new (number_of_object){rand(-100..100)}
 
 puts "my unordered array"
-p array_of_objects	
+p array_of_objects
 
 puts
 
-result = array_of_objects.msort(time)
-
+result = array_of_objects.m_merge_sort(time)
 puts "ordered array"
-p result
-
-
+p array_of_objects
+# begin
+#   result = array_of_objects.m_merge_sort(time)
+#   puts "ordered array"
+#   p result
+# rescue Test::Unit::AssertionFailedError => e
+#   # puts e.backtrace
+#   p array_of_objects
+#   p e.message
+# end
